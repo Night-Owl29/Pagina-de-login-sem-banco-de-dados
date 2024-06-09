8fa7e2dbe3e9422d27519fb99f878b365d09d375
Night_Owl <guilhermepomacercena@gmail.com>
2024-06-09 02:18:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="stylelogin.css" media="screen" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<body>
+		<div class="global">
+			<form name="" method="post" action="">
+				<label>Usuário <input type="text" name="user" /></label><br /><br />
+				<label>Senha <input type="password" name="pass" /></label><br /><br />
+				<input type="submit" name="submit" value="Logar" />
+</div>
+	<?php
+				$user = @$_REQUEST['user'];
+				$pass = @$_REQUEST['pass'];
+				$submit = @$_REQUEST['submit'];
+
+                /* Seu Nome e Senha */
+				$user1 = 'Test';
+				$pass1 = 'test';
+
+				$user2 = 'Admin';
+				$pass2 = 'admin';
+
+				if($submit){
+					if($user == "" || $pass == ""){
+						echo "<script:alert('Por favor, preencha todos os campos!');</script>";
+					}
+					else{
+						if(($user == $user1 && $pass == $pass1) || ($user == $user2 && $pass == $pass2)){
+							session_start();
+							$_SESSION['usuario']=$user;
+							$_SESSION['senha']=$pass;
+							header("Location: painel.php");
+						}
+						else{
+							echo "<script>alert('Usuário e/ou senha inválido(s), Tente novamente!');</script>";
+						}
+					}
+				}
+			?>
+
+</body>
+</html>
---
+++ painel.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="stylesheet.css" />
+  </head>
+  <body>
+    <div class="home">
+      <div class="div">
+        <div class="overlap-group">
+          <div class="rectangle"></div>
+          <div class="rectangle-2"></div>
+          <div class="text-wrapper">Loja</div>
+          <div class="rectangle-3"></div>
+          <div class="text-wrapper-2">Sobre Mim</div>
+          <div class="rectangle-4"></div>
+          <div class="text-wrapper-3">Contato</div>
+          <img class="element" src="img/40368-1.png" />
+          <img
+            class="dreamshaper-logo"
+            src="img/dreamshaper-v7-logo-para-loja-de-brinquedo-com-o-nome-nightowl-0-removebg-preview-1.png"
+          />
+          <div class="rectangle-5"></div>
+          <div class="text-wrapper-4">Pesquisar</div>
+          <img class="star" src="img/star-1.svg" />
+        </div>
+        <div class="overlap">
+          <img class="fmdkvkwqaeoei" src="img/fmdkv8kwqaeo1ei-1.png" />
+          <img class="pointing-finger" src="img/pointing-finger-1922074-1280-1.png" />
+          <img class="image" src="img/image-processing20200511-32371-j6g17u-1.png" />
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
