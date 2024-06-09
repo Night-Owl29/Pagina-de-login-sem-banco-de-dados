9a9b889003e524ad079d1996227f59a31e7d9879
Night_Owl <guilhermepomacercena@gmail.com>
2024-06-09 02:21:40 -0300

    painel.html

--- painel.php
+++ painel.php
@@ -1,34 +1,31 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" href="stylesheet.css" />
-  </head>
-  <body>
-    <div class="home">
-      <div class="div">
-        <div class="overlap-group">
-          <div class="rectangle"></div>
-          <div class="rectangle-2"></div>
-          <div class="text-wrapper">Loja</div>
-          <div class="rectangle-3"></div>
-          <div class="text-wrapper-2">Sobre Mim</div>
-          <div class="rectangle-4"></div>
-          <div class="text-wrapper-3">Contato</div>
-          <img class="element" src="img/40368-1.png" />
-          <img
-            class="dreamshaper-logo"
-            src="img/dreamshaper-v7-logo-para-loja-de-brinquedo-com-o-nome-nightowl-0-removebg-preview-1.png"
-          />
-          <div class="rectangle-5"></div>
-          <div class="text-wrapper-4">Pesquisar</div>
-          <img class="star" src="img/star-1.svg" />
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Blog</title>
+    <link rel="stylesheet" href="blog.css">
+</head>
+<body>
+    <header>
+        <h1>Meu Blog</h1>
+    </header>
+    <nav>
+        <a href="#">Início</a>
+        <a href="#">Sobre</a>
+        <a href="#">Contato</a>
+    </nav>
+    <div class="container">
+        <div class="post">
+            <h2>Título do Post 1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, lorem quis bibendum pulvinar, odio purus hendrerit leo, in accumsan dolor libero nec quam.</p>
+            <p class="date">Postado em: 09 de Junho, 2024</p>
         </div>
-        <div class="overlap">
-          <img class="fmdkvkwqaeoei" src="img/fmdkv8kwqaeo1ei-1.png" />
-          <img class="pointing-finger" src="img/pointing-finger-1922074-1280-1.png" />
-          <img class="image" src="img/image-processing20200511-32371-j6g17u-1.png" />
+        <div class="post">
+            <h2>Título do Post 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, lorem quis bibendum pulvinar, odio purus hendrerit leo, in accumsan dolor libero nec quam.</p>
+            <p class="date">Postado em: 08 de Junho, 2024</p>
         </div>
-      </div>
     </div>
-  </body>
+</body>
 </html>
